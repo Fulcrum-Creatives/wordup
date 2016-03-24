4d5dbb3b2fbc0e2e564d7eefe5b7caeab15f9c1d
jawittdesigns <Killjoy11>
2016-03-24 17:13:00 -0400

    Added new image

--- templates/template-listings.php
+++ templates/template-listings.php
@@ -3,6 +3,7 @@
 Template Name: Listings
 */
 get_header(); 
+  get_template_part( 'template-parts/partials/thumbnail' );
   get_template_part( 'template-parts/entry/title' );
   get_template_part( 'template-parts/entry/listings' );
   get_template_part( 'template-parts/partials/featured' );
